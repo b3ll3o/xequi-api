800fafe793e203294e3e427726d2eb0e7492cc1d
Leonardo Rocha Andreazzi <leonardoandreazzi@gmail.com>
2024-02-02 16:01:19 -0300

    testes unitarios auth login

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -9,17 +9,24 @@ import { ValidationPipeCustom } from '@/shared/pipes/validation.pipe.custom';
 import { UsuarioStub } from '@/usuarios/test/stubs/entities/usuario.entity.stub';
 import { Empresa } from '@/empresas/domain/entities/empresa.entity';
 import { UsuarioEmpresa } from '@/empresas/domain/entities/usuario.empresa.entity';
+import { AuthModule } from '@/auth/auth.module';
+import { JwtGuard } from '@/auth/application/guards/jwt.guard';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { Reflector } from '@nestjs/core';
 
-const BASE_URL = '/usuarios';
+const BASE_URL = '/auth';
 
-describe('usuarios', () => {
+describe('auth', () => {
   let app: INestApplication;
   let repository: Repository<Usuario>;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [
         UsuariosModule,
+        JwtModule,
+        AuthModule,
         TypeOrmModule.forRoot({
           type: 'sqlite',
           database: ':memory:',
@@ -31,44 +38,49 @@ describe('usuarios', () => {
     }).compile();
 
     repository = module.get(getRepositoryToken(Usuario));
+    jwtService = module.get(JwtService);
 
     app = module.createNestApplication();
     app.useGlobalPipes(new ValidationPipeCustom());
+    app.useGlobalGuards(new JwtGuard(jwtService, new Reflector()));
     await app.init();
   });
 
-  describe('cadastra', () => {
-    it('deve cadastrar um novo usuario', () => {
+  describe('login', () => {
+    const BASE_URL_LOGIN = `${BASE_URL}/login`;
+
+    it('deve fazer o login de um usuario já cadastrado', async () => {
+      const usuarioCadastradoSenhaHash =
+        await UsuarioStub.cadastradoHashSenha();
+      await repository.save(usuarioCadastradoSenhaHash);
       return request(app.getHttpServer())
-        .post(BASE_URL)
+        .post(BASE_URL_LOGIN)
         .send(UsuarioStub.novo())
-        .expect(201)
-        .expect({
-          id: 1,
-          email: UsuarioStub.EMAIL,
-        });
+        .expect(200);
     });
 
-    it('não deve cadastrar dois usuarios com o mesmo email', async () => {
-      await repository.save(UsuarioStub.cadastrado());
+    it('não deve fazer o loginde um usuario não cadastrado', async () => {
       return request(app.getHttpServer())
-        .post(BASE_URL)
+        .post(BASE_URL_LOGIN)
         .send(UsuarioStub.novo())
-        .expect(400);
+        .expect(401);
     });
 
-    it('não deve cadastrar um usuario sem email', () => {
+    it('não deve fazer o login de um usuario com a senha errada', async () => {
+      const usuarioCadastradoSenhaHash =
+        await UsuarioStub.cadastradoHashSenha();
+      await repository.save(usuarioCadastradoSenhaHash);
       return request(app.getHttpServer())
-        .post(BASE_URL)
-        .send(UsuarioStub.novo(new Usuario({ email: '' })))
-        .expect(400);
-    });
-
-    it('não deve cadastrar um usuario sem senha', () => {
-      return request(app.getHttpServer())
-        .post(BASE_URL)
-        .send(UsuarioStub.novo(new Usuario({ senha: '' })))
-        .expect(400);
+        .post(BASE_URL_LOGIN)
+        .send(
+          UsuarioStub.novo(
+            new Usuario({
+              email: UsuarioStub.EMAIL,
+              senha: UsuarioStub.SENHA_2,
+            }),
+          ),
+        )
+        .expect(401);
     });
   });
 
